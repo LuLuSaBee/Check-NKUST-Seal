b378672f1258dcea67279d8e4dd19bf89aee4693
lulusabee <lulusabeetw@gmail.com>
2020-04-23 00:21:16 +0800

    20200423

--- llsb_utils.py
+++ llsb_utils.py
@@ -3,8 +3,6 @@ import cv2
 import requests
 import numpy as np
 import tensorflow as tf
-import tkinter as tk
-from tkinter import ttk
 from PIL import Image
 from io import BytesIO
 from tkinter import messagebox
@@ -14,86 +12,6 @@ from object_detection import ObjectDetection
 # Due to tensorflow
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-
-class GUI():
-    def __init__(self, width=200, height=130, textFont=('Hack', 24)):
-        super().__init__()
-        handler = ActionHandler()
-        self = tk.Tk()
-        self.position_x = (self.winfo_screenwidth()/2) - (width*2)
-        self.position_y = (self.winfo_screenheight()/2) - (height*2)
-        self.title('NKUST Seal Checker')
-        self.geometry('%dx%d+%d+%d' %
-                      (width, height, self.position_x, self.position_y))
-        self.configure(background='white')
-
-        def openInputUrlForm():
-            form = tk.Tk()
-            form.title("Input URL")
-            form.geometry("+300+300")
-            form_entry = tk.Entry(form)
-            form_entry.focus()
-            form_entry.pack()
-            btn_frame = tk.Frame(form)
-            btn_frame.pack()
-            form_subbtn = tk.Button(btn_frame, text='   Enter   ', font=('Hack', 12),
-                                    command=lambda: [f() for f in [lambda: handler.btnClick(
-                                        "UrlImg", form_entry.get()), form.destroy]])
-
-            form_exitbtn = tk.Button(btn_frame, text='  Cancel  ',
-                                     font=('Hack', 12), command=form.destroy)
-            form_subbtn.pack(side=tk.LEFT)
-            form_exitbtn.pack(side=tk.LEFT)
-            form.mainloop()
-
-        def deviceList():
-            index = 0
-            webcam_list = []
-            while True:
-                if not cv2.VideoCapture(index).read()[0]:
-                    break
-                else:
-                    webcam_list.append(index)
-                index += 1
-            if index == 0:
-                messagebox.showerror(
-                    "Device Error", "You don't have Webcam device")
-                return
-            form = tk.Tk()
-            form.title("Choose DeviceID")
-            form.geometry("+400+400")
-            form_combo = ttk.Combobox(form, values=webcam_list)
-            form_combo.current(0)
-            form_combo.pack()
-            btn_frame = tk.Frame(form)
-            btn_frame.pack()
-            form_subbtn = tk.Button(btn_frame, text='   Enter   ', font=('Hack', 12),
-                                    command=lambda: [f() for f in [lambda:
-                                                                   handler.btnClick(
-                                                                       "OpenDev", deviceID=form_combo.get()),
-                                                                   form.destroy]])
-            form_exitbtn = tk.Button(btn_frame, text='  Cancel  ',
-                                     font=('Hack', 12), command=form.destroy)
-            form_subbtn.pack(side=tk.LEFT)
-            form_exitbtn.pack(side=tk.LEFT)
-            form.mainloop()
-
-        self.choose_img_btn = tk.Button(
-            self, text='Choose Image', font=textFont, command=lambda: handler.btnClick("OpenImg"))
-        self.url_img_btn = tk.Button(
-            self, text='Open  ImgUrl', font=textFont, command=openInputUrlForm)
-        self.open_dev_btn = tk.Button(
-            self, text='Open  Webcam', font=textFont, command=deviceList)
-        self.exit_btn = tk.Button(
-            self, text='    Exit    ', font=textFont, command=lambda: handler.btnClick("Exit"))
-
-        self.choose_img_btn.pack()
-        self.url_img_btn.pack()
-        self.open_dev_btn.pack()
-        self.exit_btn.pack()
-        self.protocol("WM_DELETE_WINDOW", lambda: handler.btnClick("Exit"))
-        self.mainloop()
 
 
 class ActionHandler():
@@ -115,6 +33,10 @@ class ActionHandler():
 
     def __getFile(self):
         file_path = filedialog.askopenfilename()
+        if file_path == '':
+            messagebox.showerror(
+                "Error", "Please choose File")
+            return
         try:
             img = Image.open(file_path)
             self.magistrate.showImage(img=img)
@@ -149,8 +71,10 @@ class ActionHandler():
         cv2.destroyAllWindows()
 
     def __askQuit(self):
-        # if messagebox.askokcancel("Quit", "Do you really wish to quit?"):
-        sys.exit()
+        if messagebox.askokcancel("Quit", "Do you really wish to quit?"):
+            sys.exit()
+        else:
+            pass
 
 
 class Magistrate():
@@ -193,12 +117,13 @@ class Magistrate():
 
     def __judge(self, img):
         handPredictions = self.__isHandJudge(Image.fromarray(img))
-        img = cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
+        # img = np.asarray(img)
+        print(handPredictions)
         for hand_pred_ret in handPredictions:
             prob = hand_pred_ret['probability']
             tagID = hand_pred_ret['tagId']
             if tagID == 0 and prob > 0.2:
-                print("{tagID} {prob}")
+                print(f"{hand_pred_ret['tagName']} {prob}")
                 bbox = hand_pred_ret['boundingBox']
                 left = bbox['left']
                 top = bbox['top']
@@ -208,13 +133,14 @@ class Magistrate():
                 y1 = int(top * img.shape[0])
                 x2 = x1 + int(width * img.shape[1])
                 y2 = y1 + int(height * img.shape[0])
-                isHand = True
-                sealPredictions = self.__isSealJudge(Image.fromarray(img))
+                cv2.imshow('test', img[y1:y2, x1:x2])
+                sealPredictions = self.__isSealJudge(
+                    Image.fromarray(img[y1:y2, x1:x2]))
                 for seal_pred_ret in sealPredictions:
                     seal_prob = seal_pred_ret['probability']
                     seal_tagID = seal_pred_ret['tagId']
                     if seal_tagID == 0 and seal_prob > 0.2:
-                        print("{tagID} {prob}")
+                        print(f"{seal_pred_ret['tagName']} {prob}")
                         return "Pass", (0, 255, 0)
 
         return "None", (0, 0, 0)
--- main.py
+++ main.py
@@ -1,4 +1,88 @@
-from llsb_utils import GUI
+import cv2
+import tkinter as tk
+from tkinter import ttk
+from llsb_utils import ActionHandler
+
+
+class GUI():
+    def __init__(self, width=200, height=130, textFont=('Hack', 24)):
+        super().__init__()
+        handler = ActionHandler()
+        self = tk.Tk()
+        self.position_x = (self.winfo_screenwidth()/2) - (width*2)
+        self.position_y = (self.winfo_screenheight()/2) - (height*2)
+        self.title('NKUST Seal Checker')
+        self.geometry('%dx%d+%d+%d' %
+                      (width, height, self.position_x, self.position_y))
+        self.configure(background='white')
+
+        def openInputUrlForm():
+            form = tk.Tk()
+            form.title("Input URL")
+            form.geometry("+300+300")
+            form_entry = tk.Entry(form)
+            form_entry.focus()
+            form_entry.pack()
+            btn_frame = tk.Frame(form)
+            btn_frame.pack()
+            form_subbtn = tk.Button(btn_frame, text='   Enter   ', font=('Hack', 12),
+                                    command=lambda: [f() for f in [lambda: handler.btnClick(
+                                        "UrlImg", form_entry.get()), form.destroy]])
+
+            form_exitbtn = tk.Button(btn_frame, text='  Cancel  ',
+                                     font=('Hack', 12), command=form.destroy)
+            form_subbtn.pack(side=tk.LEFT)
+            form_exitbtn.pack(side=tk.LEFT)
+            form.mainloop()
+
+        def deviceList():
+            index = 0
+            webcam_list = []
+            while True:
+                if not cv2.VideoCapture(index).read()[0]:
+                    break
+                else:
+                    webcam_list.append(index)
+                index += 1
+            if index == 0:
+                messagebox.showerror(
+                    "Device Error", "You don't have Webcam device")
+                return
+            form = tk.Tk()
+            form.title("Choose DeviceID")
+            form.geometry("+400+400")
+            form_combo = ttk.Combobox(form, values=webcam_list)
+            form_combo.current(0)
+            form_combo.pack()
+            btn_frame = tk.Frame(form)
+            btn_frame.pack()
+            form_subbtn = tk.Button(btn_frame, text='   Enter   ', font=('Hack', 12),
+                                    command=lambda: [f() for f in [lambda:
+                                                                   handler.btnClick(
+                                                                       "OpenDev", deviceID=form_combo.get()),
+                                                                   form.destroy]])
+            form_exitbtn = tk.Button(btn_frame, text='  Cancel  ',
+                                     font=('Hack', 12), command=form.destroy)
+            form_subbtn.pack(side=tk.LEFT)
+            form_exitbtn.pack(side=tk.LEFT)
+            form.mainloop()
+
+        self.choose_img_btn = tk.Button(
+            self, text='Choose Image', font=textFont, command=lambda: handler.btnClick("OpenImg"))
+        self.url_img_btn = tk.Button(
+            self, text='Open  ImgUrl', font=textFont, command=openInputUrlForm)
+        self.open_dev_btn = tk.Button(
+            self, text='Open  Webcam', font=textFont, command=deviceList)
+        self.exit_btn = tk.Button(
+            self, text='    Exit    ', font=textFont, command=lambda: handler.btnClick("Exit"))
+
+        self.choose_img_btn.pack()
+        self.url_img_btn.pack()
+        self.open_dev_btn.pack()
+        self.exit_btn.pack()
+        self.protocol("WM_DELETE_WINDOW", lambda: handler.btnClick("Exit"))
+        self.mainloop()
+
 
 if __name__ == '__main__':
     mygui = GUI()
